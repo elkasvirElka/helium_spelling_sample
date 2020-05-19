f3caa967ffc1c026b1e9283d5a90af47f01ab8a0
elkasvirElka <e.minnullina2015@gmail.com>
2020-05-19 15:00:23 -0500

    git package

--- app/src/test/java/com/elviraminnullina/helium_samples/ExampleUnitTest.kt
+++ app/src/test/java/com/elviraminnullina/helium_samples/ExampleUnitTest.kt
@@ -1,14 +1,8 @@
 package com.elviraminnullina.helium_samples
 
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.UnstableDefault
 import org.junit.Test
-import kotlinx.serialization.json.Json.Default.parse
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.json.Json.Default.stringify
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -35,15 +29,15 @@ class ExampleUnitTest {
 
         val json = "{\"data\":{\"text\":\"test\"}}"
         //Kotlin serialization
-      /*  val dataSerializer: KSerializer<Data> = Data.serializer()
-        val boxedDataSerializer: KSerializer<Container<Data>> = Container.serializer(dataSerializer)
-        val kotlinParsed = parse(boxedDataSerializer, json)
+        /*  val dataSerializer: KSerializer<Data> = Data.serializer()
+          val boxedDataSerializer: KSerializer<Container<Data>> = Container.serializer(dataSerializer)
+          val kotlinParsed = parse(boxedDataSerializer, json)
 
-        //Gson serialization
-        val collectionType = object : TypeToken<Container<Data>>() {}.type
-        val gsonParsed = Gson().fromJson<Container<Data>>(json, collectionType)
+          //Gson serialization
+          val collectionType = object : TypeToken<Container<Data>>() {}.type
+          val gsonParsed = Gson().fromJson<Container<Data>>(json, collectionType)
 
-        assertEquals("test", kotlinParsed.data.text)
-        assertEquals(gsonParsed.data.text, kotlinParsed.data.text)*/
+          assertEquals("test", kotlinParsed.data.text)
+          assertEquals(gsonParsed.data.text, kotlinParsed.data.text)*/
     }
 }
